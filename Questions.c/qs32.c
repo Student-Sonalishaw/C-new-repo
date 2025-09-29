5165871acdab6d98299785773fc3c9810ce3e2c4
Student-Sonalishaw <sonalishaw311@gmail.com>
2025-09-29 10:53:15 +0530

    today's first commit

---
+++ Questions.c/qs32.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+//check if a string is a palindrome.(palindrome = original and reverse is same)
+int main(){
+    char str[1000];
+    int madamflag = 0;
+    printf("Enter a string: ");
+    scanf("%s",&str);
+
+    int len = strlen(str);
+
+    for(int i = 0; i < len/2; i++){
+        if(str[i] != str[len-i-1]){
+            flag = 1;
+            break;
+        }
+    }
+
+    if(flag == 0){
+        printf("The string is a palindrome.\n");
+    } else{
+        printf("The string is not a palindrome.\n");
+    }
+
+
+
+    return 0;
+}
